d590d7b626ae80892a61ba733b4407d019c9007e
alfian <alfianwilfredohoris@gmail.com>
2023-01-31 21:44:06 +0700

    commit

---
+++ client-user/src/components/navbar.jsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import Image from "next/image";
+import logo from "@/public/logonav.png";
+import logom from "@/public/log.png";
+import { useState } from "react";
+
+export default function Navbar() {
+  const [isHov, setIsHov] = useState(false);
+  return (
+    <>
+      <div className="w-screen flex flex-row text-[#EEEEEE] bg-[#222831] h-[100px]">
+        <div className="px-[60px] grid content-center w-full">
+          <div className="flex justify-between w-full">
+            <div
+              onMouseEnter={() => setIsHov(true)}
+              onMouseLeave={() => setIsHov(false)}
+            >
+              {isHov ? (
+                <Image src={logom} className=" w-[80px] h-[80px]" />
+              ) : (
+                <Image src={logo} className=" w-[80px] h-[80px]" />
+              )}
+            </div>
+            <div className="grid content-center">
+              <div className="flex flex-row gap-[10px]">
+                <div>
+                  <button className="outline outline-1 outline-[#00ADB5] rounded-[1px] w-[90px] h-[40px] hover:bg-[#00ADB5] text-[#EEEEEE] transition-colors duration-[1200ms]">
+                    Login
+                  </button>
+                </div>
+                <div>
+                  <button className="outline outline-1 outline-[#00ADB5] rounded-[1px] w-[90px] h-[40px] hover:bg-[#00ADB5] hover:text-[#222831] text-[#EEEEEE] transition-colors duration-[1200ms]">
+                    Register
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- client-user/src/pages/index.js
+++ client-user/src/pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import styles from "@/styles/Home.module.css";
+import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
@@ -11,6 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
+        <Navbar />
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </main>
     </>
