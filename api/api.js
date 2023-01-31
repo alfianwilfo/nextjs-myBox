70dc5e50669c45d25162808f9fef99a5442906d5
alfian <alfianwilfredohoris@gmail.com>
2023-01-31 19:02:00 +0700

    init next js

--- api/api.js
+++ api/api.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3001;
 const route = require("./routes/");
 const cors = require("cors");
 const errorHandler = require("./helpers/errorHandler");
