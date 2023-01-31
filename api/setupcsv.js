0cd6ca3bce3afa0c6a50ea07fb2e31d0593ec103
alfian <alfianwilfredohoris@gmail.com>
2023-01-31 14:03:17 +0700

    delete unnecessary file

--- api/setupcsv.js
+++
@@ -1,8 +0,0 @@
-let csvToJson = require("convert-csv-to-json");
-let fileInputName = "ibox-product.csv";
-let fileOutputName = "product.json";
-
-let json = csvToJson.getJsonFromCsv(fileInputName);
-for (let i = 0; i < json.length; i++) {
-  console.log(json[i]);
-}
