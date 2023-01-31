569cc10004b7ec4a7e723e578a466041587d0a71
alfian <alfianwilfredohoris@gmail.com>
2023-01-31 17:19:29 +0700

    working on login

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -9,5 +9,13 @@ class User {
       console.log(error);
     }
   }
+
+  static async login(req, res, next) {
+    try {
+      let { email, password } = req.body;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 module.exports = User;
