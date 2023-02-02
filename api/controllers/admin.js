9af953461c9d94be71a04fb1d4cea6ce2f3bd1ff
alfian <alfianwilfredohoris@gmail.com>
2023-02-02 12:59:52 +0700

    check

--- api/controllers/admin.js
+++ api/controllers/admin.js
@@ -1,8 +1,23 @@
+let { admin } = require("../models/");
 class Admin {
   static async login(req, res, next) {
     try {
       let { username, password } = req.body;
-      console.log(username, password);
+      if (!username) {
+        throw {
+          name: "validator",
+          status: 400,
+          message: "Username can't empty",
+        };
+      }
+      if (!password) {
+        throw {
+          name: "validator",
+          status: 400,
+          message: "Password can't empty",
+        };
+      }
+      let findedUser = await admin.findOn({ where: { username } });
     } catch (error) {
       console.log(error);
     }
